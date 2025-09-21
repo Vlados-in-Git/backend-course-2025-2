496c4d11a8f9d85a380e03f5adb9871034300222
Vlad <3vlad201711111111@gmail.com>
2025-09-21 15:47:08 +0300

    Add a program which construct API

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
+const url = require ("node:url");
+
+const serch = "NBU_ovdp?"
+const type = "json";
+const currency = "EUR";
+const exact_date = "10.09.2024";
+
+const myUrl = new URL ('https://bank.gov.ua/NBU_ovdp?json');
+
+
+myUrl.searchParams.set("date", exact_date);
+myUrl.searchParams.set("val_code", currency);
+
+// {serch}${type}&date=${exact_date}&val_code=${currency}');
+
+console.log(myUrl.href);
 
